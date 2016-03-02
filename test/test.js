1c8f08e6bdbc8af7a26db78de793ca0b72fba3f8
rome-bop <park.srome@gmail.com>
2016-03-01 18:48:38 -0800

    fixed adding coveralls to package.json

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var bopular = require('../index');
 
 describe('#bopular', function() {
-  it('should not return unefined', function() {
+  it('should not return undefined', function() {
     var result = bopular();
     expect(result).to.equal(undefined);
   });
